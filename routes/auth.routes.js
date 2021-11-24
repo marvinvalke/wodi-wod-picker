eb6510c4f235b41c7dafb2bf08c622a7ccfbe1fc
vntero <shin.antero@gmail.com>
2021-11-24 12:50:12 +0100

    user CRUD done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcryptjs");
 
 //---------------------------------------ROUTES------------------------------------
 
-//-----------------REGISTER-----------------
+//-----------------REGISTER--------------------
 // GET for register
 router.get("/register", (req, res, next) => {
   //SHOW THE FORM FOR REGISTERING
@@ -25,7 +25,7 @@ router.post("/register", (req, res, next) => {
   //create the user in our db
   UserModel.create({name, email, password: hash})
     .then(() => {
-      res.redirect("/");
+      res.redirect("/welcome");
       console.log('*****USER CREATED*****')
     })
     .catch((err) => {
@@ -34,7 +34,7 @@ router.post("/register", (req, res, next) => {
     });
 });
 
-//-------------------LOGIN-------------------
+//-------------------LOGIN---------------------
 // GET for login
 router.get("/login", (req, res, next) =>{
   res.render("auth/login.hbs")
@@ -50,7 +50,7 @@ UserModel.find({email})
     if (userResponse.length){
       console.log(userResponse);
       //decryption
-      let userObj = userResponse[2];
+      let userObj = userResponse[0];
       //do passwords match?
       let isMatching = bcrypt.compareSync(password, userObj.password);
       if (isMatching) {
@@ -73,6 +73,98 @@ UserModel.find({email})
     next(err)
   })
 });
+// //-------------------------------PROTECTED ROUTES------------------------------------
+//----------- CHECK IF LOGGED IN --------------
+const isLoggedIn = (req, res, next) => {
+  if (req.session.myProperty) {
+    // IF LOG IN SUCCESSFUL -> GO TO THE GET /WELCOME
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+router.get("/welcome", isLoggedIn, (req, res, next) => {
+  let theUsername = req.session.myProperty;
+  res.render("auth/welcome.hbs", {
+    username: theUsername.username
+  });
+});
+router.get("/logout",  (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+
+router.get("/profile", isLoggedIn,  (req, res) => {
+  res.render("auth/profile.hbs")
+});
+
+//------------ EDIT USER PROFILE --------------
+router.get("/profile/:id/edit", (req, res, next) =>{
+  const {id} = req.params
+  UserModel.findById(id)
+  .then((user)=>{
+    res.render("/auth/profile-update.hbs", user)
+  })
+  .catch((err) =>{
+    next(err)
+  })
+})
+
+router.get('/drones/:id/edit', (req, res, next) => {
+  // Iteration #4: Update the drone
+  const {
+    id
+  } = req.params
+  DroneModel.findById(id)
+    .then((drone) => {
+      console.log(drone)
+      res.render("drones/update-form", drone)
+    })
+    .catch((err) => {
+      console.log("Something went wrong: " + err)
+    })
+}); //#4
+
+// router.post('/drones/:id/edit', (req, res, next) => {
+//   // Iteration #4: Update the drone
+//   const {
+//     name,
+//     propellers,
+//     maxSpeed
+//   } = req.body
+//   const {
+//     id
+//   } = req.params
+//   DroneModel.findByIdAndUpdate(id, {
+//       name: name,
+//       propellers: propellers,
+//       maxSpeed: maxSpeed
+//     })
+//     .then(() => {
+//       res.redirect("/drones")
+//     })
+//     .catch((err) => {
+//       console.log("Something went wrong: " + err)
+//     })
+// }); //#4
+
+// router.post('/drones/:id/delete', (req, res, next) => {
+//   // Iteration #5: Delete the drone
+//   const {
+//     id
+//   } = req.params
+//   DroneModel.findByIdAndDelete(id)
+//     .then(() => {
+//       console.log("Drone deleted!")
+//       res.redirect("/drones")
+//     })
+//     .catch((err) => {
+//       console.log("Something went wrong: " + err)
+//     })
+// });
+
+
 
 // EXPORT THE ROUTES
 module.exports = router;
